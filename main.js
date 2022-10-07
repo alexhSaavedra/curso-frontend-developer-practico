de89a6f9558b63a7faafe795a5b776a2c02de852
Alex Saavedra <alexsaavedra.contacto@gmail.com>
2022-10-07 12:53:45 -0400

    se añadio el aside pero faltan cmabios en css y problemas de open and close entre dos componentes

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const menuListIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const navShoppingCart = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuListIcon.addEventListener("click", toggleMobileMenu);
+navShoppingCart.addEventListener("click", togleCarritoAside);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
@@ -12,4 +15,7 @@ function toggleDesktopMenu() {
 
 function toggleMobileMenu() {
   mobileMenu.classList.toggle("inactive");
+}
+function togleCarritoAside() {
+  aside.classList.toggle("inactive");
 }
